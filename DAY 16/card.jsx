009883c55efbb7dcb843af6bde4bd523c82ed59c
Abhijay Krishna <84517361+AbhijayKrishna01@users.noreply.github.com>
2021-07-12 16:16:57 +0530

    Add files via upload

---
+++ DAY 16/card.jsx
@@ -0,0 +1,10 @@
+const Card = ()=>{
+    return (
+        <div className="card">
+            <img src="https://yt3.ggpht.com/-7zFDHK5X45w/AAAAAAAAAAI/AAAAAAAAAAA/QJfHeLTEZwE/s900-c-k-no-mo-rj-c0xffffff/photo.jpg"/>
+          <h2>React Card</h2>
+          <p>This is a Card</p>
+        </div>
+    );
+};
+export default Card;
